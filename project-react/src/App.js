5d8fddba2e1ba963b37071c795b379f2176b7ab4
5h6sm <5h6sm.github@gmail.com>
2023-03-10 11:39:31 +0900

    && 연산자 1

--- project-react/src/App.js
+++ project-react/src/App.js
@@ -2,12 +2,8 @@ import './App.css';
 
 function App() {
   const name = '리액트';
-  return (
-    <div>
-      {name === '리액트'?<h1>리액트입니다.</h1>:null}
-      
-    </div>
-  );
+  return <div> 
+    { name === '리액트' && <h1> 리액트입니다.</h1>}
+  </div>
 }
-
 export default App;
